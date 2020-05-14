5f1c0740360a4684c077a442b033d99d7ccbb14b
Lalala <1768365755@qq.com>
2020-05-15 00:54:03 +0800

    Cap4

---
+++ Cap4/Cap4.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+
+typedef char(*AP)[5];
+AP foo(char* p) {
+    for (int i = 0; i < 3; i++) {
+        p[strlen(p)] = 'A';
+    }
+    return (AP)p + 1;
+}
+int main() {
+    using namespace std;
+    char s[] = "FROG\0SEAL\0LION\0LAMB"; 
+//    puts(foo(s)[1] + 2);
+    cout << foo(s)[1][0];
+}
+
+//int main()
+//{
+//	using namespace std;
+//
+//	//释放空指针是安全的
+//	int* o = nullptr;
+//	delete o;
+//
+//	delete[]a;
+//	
+//
+//
+//	//delete p;
+//	
+//	return 0;
+//}
